c8edde9827856f4e5b39ed625fcd347194945291
Luke Bezzina <luke.bezzina.18@um.edu.mt>
2021-06-26 17:27:08 +0200

    Final Version

--- batch_run.py
+++ batch_run.py
@@ -1,8 +1,23 @@
+import glob
+import os
+import time
 import papermill as pm
+import ntpath
 
-pm.execute_notebook('preprocessDataLSTM_Trends_3dprice.ipynb','out.ipynb', parameters = {"trend_days":3})
-pm.execute_notebook('preprocessDataLSTM_Trends_3dprice.ipynb','out.ipynb', parameters = {"trend_days":5})
-pm.execute_notebook('preprocessDataLSTM_Trends_3dprice.ipynb','out.ipynb', parameters = {"trend_days":7})
-pm.execute_notebook('preprocessDataLSTM_Trends_3dprice.ipynb','out.ipynb', parameters = {"trend_days":9})
-pm.execute_notebook('preprocessDataLSTM_Trends_3dprice_normalized.ipynb','out.ipynb', parameters = {"trend_days":3})
-pm.execute_notebook('preprocessDataLSTM_3dprice.ipynb','out.ipynb')
+
+ROOT_DIR = os.path.abspath(os.curdir)
+
+path_trends = ROOT_DIR + '\\trends'
+csvfiles = glob.glob(path_trends + "/*.csv")
+
+# for j, file in enumerate(csvfiles):
+#     filename = ntpath.basename(file)
+#     trd_variant = filename.split("GoogleTrendsAdjusted",1)[1]
+#     trd_variant = trd_variant[:-4] # up until ".csv"
+#     pm.execute_notebook('preprocessData10d_3rdP_GoogleTrd.ipynb', 'out.ipynb', parameters={ "trend_file_name": filename, "trends_variant": trd_variant})
+
+for i, file in enumerate(csvfiles):
+    filename = ntpath.basename(file)
+    trd_variant = filename.split("GoogleTrendsAdjusted",1)[1]
+    trd_variant = trd_variant[:-4]
+    pm.execute_notebook('preprocessDataLSTM_Trends_3dprice_GoogleTrd.ipynb', 'out.ipynb', parameters={"trend_days": 7, "trend_file_name": filename, "trends_variant": trd_variant})
